7ad5e563ba23d89590e7ac0ac373f87c1cb18f35
chenaoh <adonis.lopez@epn.edu.ec>
2025-07-18 19:41:20 -0500

    Se agrega el proyecto

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ProyectoProgramacion_FILE C:/Qt/Proyects/ProyectoProgramacion/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/ProyectoProgramacion.exe)
+set(__QT_DEPLOY_TARGET_ProyectoProgramacion_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ProyectoProgramacion_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
