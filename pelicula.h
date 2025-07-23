dd5eda70311b9b9809283ec9b82c0cc47f56425d
Adonis_Lopez <adonis.lopez@epn.edu.ec>
2025-07-23 11:15:27 -0500

    Se crea el struct de la pelicula

--- pelicula.h
+++ pelicula.h
@@ -1,4 +1,14 @@
 #ifndef PELICULA_H
 #define PELICULA_H
-
+#include <Qstring>
 #endif // PELICULA_H
+
+struct pelicula {
+    int posicion;
+    QString titulo;
+    float calificacion;
+    int anio;
+    QString estado;
+    QString director;
+
+};
