4521d91407b4d6cfb2f52e3857ab18888a68d404
SaithC19 <saith.cajamarca@epn.edu.ec>
2025-07-28 21:56:13 -0500

    Update editarpeliculadialog.cpp

--- editarpeliculadialog.cpp
+++ editarpeliculadialog.cpp
@@ -1,9 +1,11 @@
 #include "editarpeliculadialog.h"
 #include "ui_editarpeliculadialog.h"
+#include <QMessageBox>  
 
 EditarPeliculaDialog::EditarPeliculaDialog(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::EditarPeliculaDialog)
+    ui(new Ui::EditarPeliculaDialog),
+    m_debeEliminar(false)  
 {
     ui->setupUi(this);
 }
@@ -13,20 +15,40 @@ EditarPeliculaDialog::~EditarPeliculaDialog()
     delete ui;
 }
 
+
+bool EditarPeliculaDialog::debeEliminar() const
+{
+    return m_debeEliminar;
+}
+
+void EditarPeliculaDialog::on_btnEliminar_clicked()
+{
+    QMessageBox::StandardButton respuesta;
+    respuesta = QMessageBox::question(this, 
+                                    "Confirmar eliminación",
+                                    "¿Estás seguro de eliminar esta película?",
+                                    QMessageBox::Yes|QMessageBox::No);
+    
+    if(respuesta == QMessageBox::Yes) {
+        m_debeEliminar = true;
+        this->accept();
+    }
+}
+
+void EditarPeliculaDialog::on_btnGuardar_clicked()
+{
+    if(ui->lineEditTitulo->text().trimmed().isEmpty()) {
+        QMessageBox::warning(this, "Error", "El título no puede estar vacío");
+        return;
+    }
+    this->accept();
+}
+
+
 void EditarPeliculaDialog::cargarDatos(const Pelicula &pelicula) {
-    ui->lineEditTitulo->setText(pelicula.titulo);
-    ui->lineEditDirector->setText(pelicula.director);
-    ui->spinBoxAnio->setValue(pelicula.anio.toInt());
-    ui->doubleSpinBoxCalificacion->setValue(pelicula.calificacion);
-    ui->comboBoxEstado->setCurrentText(pelicula.estado);
+    
 }
 
 Pelicula EditarPeliculaDialog::obtenerDatos() const {
-    Pelicula p;
-    p.titulo = ui->lineEditTitulo->text();
-    p.director = ui->lineEditDirector->text();
-    p.anio = ui->spinBoxAnio->text();
-    p.calificacion = ui->doubleSpinBoxCalificacion->value();
-    p.estado = ui->comboBoxEstado->currentText();
-    return p;
+   
 }
